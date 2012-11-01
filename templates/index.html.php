e778c200d695c0a69381b56d666fd63a9618df73
sandeepshetty <sandeep.shetty@gmail.com>
2012-11-02 01:44:36 +0530

    Increased profile pic width and removed drafts for now.

--- templates/index.html.php
+++ templates/index.html.php
@@ -20,16 +20,13 @@ function gravatar($email, $s=80, $d='mm', $r='g', $img=false, $atts=array())
 	<div class="row">
 		<div class="span3 sidebar">
 			<div class="about">
-				<?php echo gravatar($user['email'], 420, 'mm', 'g', true, array('class'=>'img-polaroid', "width"=>175)) ?>
+				<?php echo gravatar($user['email'], 420, 'mm', 'g', true, array('class'=>'img-polaroid', "width"=>180)) ?>
 					<h3 class="author-name"><?php echo isset($user['name']) ? $user['name'] : '' ?></h3>
 					<p class="author-bio"><?php echo isset($user['bio']) ? $user['bio'] : ''?></p>
 			</div>
 
 			<ul class="unstyled channels">
 				<li><a href="#" class="active channel">Home (All Channels) <i class="icon-chevron-right icon-white pull-right"></i></a></li>
-				<?php if (isset($_SESSION['user'])) : ?>
-				<li><a class="channel" href="posts/drafts">Drafts <i class="icon-chevron-right pull-right"></i></a></li>
-				<?php endif; ?>
 				<?php foreach ($channels as $channel): ?>
 				<li><a class="channel" href="channels/<?php echo $channel['name'] ?>"><span class="hash">#</span><?php echo $channel['name'] ?> <i class="icon-chevron-right pull-right"></i></a></li>
 
@@ -38,7 +35,6 @@ function gravatar($email, $s=80, $d='mm', $r='g', $img=false, $atts=array())
 
 		</div>
 		<div class="span9 content">
-
 			<?php foreach ($posts as $post): ?>
 			<div class="post">
 				<?php echo $post['content'] ?>
