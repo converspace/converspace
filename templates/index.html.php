194f2d7fbacd69b47c1918d86db8922bd1a75e62
sandeepshetty <sandeep.shetty@gmail.com>
2012-11-02 03:43:16 +0530

    Hacking up the new design that works well with individual posts as well. It's broken ATM.

--- templates/index.html.php
+++ templates/index.html.php
@@ -18,29 +18,47 @@ function gravatar($email, $s=80, $d='mm', $r='g', $img=false, $atts=array())
 
 ?>
 	<div class="row">
-		<div class="span3 sidebar">
-			<div class="about">
-				<?php echo gravatar($user['email'], 420, 'mm', 'g', true, array('class'=>'img-polaroid', "width"=>180)) ?>
-					<h3 class="author-name"><?php echo isset($user['name']) ? $user['name'] : '' ?></h3>
-					<p class="author-bio"><?php echo isset($user['bio']) ? $user['bio'] : ''?></p>
+		<div class="span12">
+			<div class="author clearfix">
+				<div class="media">
+					<a class="pull-left" href="">
+						<?php echo gravatar($user['email'], 420, 'mm', 'g', true, array('class'=>'media-object img-polaroid', "width"=>80)) ?>
+					</a>
+					<div class="media-body">
+						<h3 class="author-name media-heading"><?php echo isset($user['name']) ? $user['name'] : '' ?> <small>(sandeep.io)</small></h3>
+						<p class="author-bio" style="color: #ccc;"><?php echo isset($user['bio']) ? $user['bio'] : ''?></p>
+					</div>
+				</div>
 			</div>
-
-			<ul class="unstyled channels">
-				<li><a href="#" class="active channel">Home (All Channels) <i class="icon-chevron-right icon-white pull-right"></i></a></li>
-				<?php foreach ($channels as $channel): ?>
-				<li><a class="channel" href="channels/<?php echo $channel['name'] ?>"><span class="hash">#</span><?php echo $channel['name'] ?> <i class="icon-chevron-right pull-right"></i></a></li>
-
-				<?php endforeach; ?>
-			</ul>
-
 		</div>
+	</div>
+	<div class="row">
 		<div class="span9 content">
+
 			<?php foreach ($posts as $post): ?>
 			<div class="post">
 				<?php echo $post['content'] ?>
 				<div class="post-permalink post-time muted"><a href="posts/<?php echo $post['id'] ?>"><?php echo $post['created_at'] ?></a></div>
 			</div>
 			<?php endforeach; ?>
+
+		</div>
+
+		<div class="span3 sidebar">
+		<!--
+			<div class="about">
+				<?php echo gravatar($user['email'], 420, 'mm', 'g', true, array('class'=>'img-polaroid', "width"=>180)) ?>
+					<h3 class="author-name"><?php echo isset($user['name']) ? $user['name'] : '' ?></h3>
+					<p class="author-bio"><?php echo isset($user['bio']) ? $user['bio'] : ''?></p>
+			</div>
+		-->
+			<ul class="unstyled channels">
+				<li><a href="#" class="channel active"><i class="icon-chevron-left icon-white"></i> Home (All Channels)</a></li>
+				<?php foreach ($channels as $channel): ?>
+				<li><a class="channel" href="channels/<?php echo $channel['name'] ?>"><i class="icon-chevron-left"></i> <span class="hash">#</span><?php echo $channel['name'] ?></a></li>
+
+				<?php endforeach; ?>
+			</ul>
 
 		</div>
 
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -12,7 +12,7 @@
 	<style>
 
 		html, body {min-height: 100%;}
-		.container { min-height: 100%; background: url(assets/img/separator.png) repeat-y 220px top;}
+		.container { min-height: 100%; /*background: url(assets/img/separator.png) repeat-y 220px top;*/}
 
 		.about {margin-bottom: 30px;}
 		.author-name { letter-spacing: -1px; margin-bottom: 8px; line-height: 1em;}
@@ -21,30 +21,44 @@
 		h1, h2, h3, h4, h5, h6 {color: #5A5A5A;}
 		author-bio {color: #666;}
 
-		.sidebar { padding: 30px 0;} /* border-right: 1px solid #eee; */
-		.content {margin-left: 0; padding: 30px 0;}
-		.post {padding: 30px; border-bottom: 1px solid #eee; font-size: 1.4em;}
-		.post:first-child, .post:first-child h1 {margin-top: 0; padding-top: 0;}
+		.sidebar { margin: 0; padding: 0;}
+		.content {margin: 0; padding: 0;}
+		.post, .author {padding: 30px; border-bottom: 1px solid #eee; font-size: 1.4em;}
+		/*.post:first-child, .post:first-child h1 {margin-top: 0; padding-top: 0;}*/
 		/* .post:last-child {border-bottom: 0 none;} */
 		.post-permalink a { font-size: 0.6em; color: #999;}
 		.post h1 {margin-bottom: 20px;}
 
-		.channels { border-top: 1px solid #EEE; margin-bottom: 0;}
+		.channels { border-left: 1px solid #EEE; margin-bottom: 0;}
 
 		.channels li {padding: 0; margin: 0;}
 		.channels a {text-decoration: none;}
 		.channel {display: block; padding: 8px 14px; text-decoration: none;}
-		.channel .icon-chevron-right {opacity: 0.25;}
+		.channel .icon-chevron-left {opacity: 0.25;}
 		.channels .active, .channels .active:hover {background-color: #08C; color: white;}
-		.channels .active .icon-chevron-right, .channels .active:hover .icon-chevron-right {opacity: 1;}
+		.channels .active .icon-chevron-left, .channels .active:hover .icon-chevron-right {opacity: 1;}
 		.channel:hover {background-color: whiteSmoke; color: #005580;}
-		.channel:hover .icon-chevron-right {opacity: 0.5;}
+		.channel:hover .icon-chevron-left {opacity: 0.5;}
 		.hash {color: #08C; opacity: 0.6; font-weight: lighter;}
 
 	</style>
 
 </head>
 <body>
+
+<?php if (isset($_SESSION['user'])) : ?>
+<div class="navbar navbar-static-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<ul class="nav">
+				<li class="active"><a href="#">Public</a></li>
+				<li><a href="#private">Private</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php endif; ?>
+
 <div class="container">
 <?php echo $content; ?>
 </div>
