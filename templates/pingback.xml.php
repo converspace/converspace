705cc0d5825070d62c10141bb6ae92e69ce13465
sandeepshetty <sandeep.shetty@gmail.com>
2013-06-10 03:19:52 +0530

    Pingback template thanks to @aaronpk

---
+++ templates/pingback.xml.php
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<methodCall>
+<methodName>pingback.ping</methodName>
+<params>
+ <param>
+  <value>
+   <string>$source</string>
+  </value>
+ </param>
+ <param>
+  <value>
+   <string>$target</string>
+  </value>
+ </param>
+</params>
+</methodCall>
