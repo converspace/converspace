85c976c762a102fb38e30dabf6a4447635e990c3
sandeepshetty <sandeep.shetty@gmail.com>
2013-06-18 08:46:50 +0530

    Hacked my way out of Bootstrap and onto Skeleton. Lots to do still but it's a start :)

--- templates/index.html.php
+++ templates/index.html.php
@@ -1,10 +1,94 @@
-<?php
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
 
-	$channels = db_get_channels($authorized);
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title><?php echo SITE_TITLE ?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-?>
-	<div class="row" id="stream-content">
-		<div class="span9 content">
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="<?php echo SITE_BASE_URL ?>css/base.css">
+	<link rel="stylesheet" href="<?php echo SITE_BASE_URL ?>css/skeleton.css">
+	<link rel="stylesheet" href="<?php echo SITE_BASE_URL ?>css/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<!-- link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png" -->
+
+
+	<!--WEB FONTS-->
+    <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+    <link href="http://fonts.googleapis.com/css?family=Merriweather:300,400" rel="stylesheet" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css' />
+
+
+	<?php if (isset($individual_post)) : ?>
+	<link href="<?php echo SITE_BASE_URL.'webmention' ?>" rel="http://webmention.org/" />
+	<?php endif; ?>
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	================================================== -->
+	<div id="nav">
+		<div class="container">
+			<div class="twelve columns offset-by-two clearfix">
+				<?php $channels = db_get_channels($authorized); ?>
+				<?php foreach ($channels as $channel): ?>
+					<a class="foobar" href=""><span class="deem">#</span><?php echo $channel['name'] ?></a>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
+
+	<div id="header">
+		<?php if (isset($_SESSION['persona'])) : ?>
+		<div class="float-menu">
+			<script> var $loggedInUser = "<?php echo $_SESSION['persona']['email'] ?>"; </script>
+			<a href="#" id="signout">Logout</a>
+		</div>
+		<?php else: ?>
+		<div class="float-menu">
+			<script> var $loggedInUser = null; </script>
+			<a href="#" id="signin">Login</a>
+		</div>
+		<?php endif; ?>
+
+
+		<div class="container">
+			<div class="h-card twelve columns offset-by-two">
+				<a class="u-url" href="<?php echo SITE_BASE_URL ?>">
+					<img class="u-photo" src="<?php echo gravatar_url(USER_EMAIL, 420, 'mm', 'g', true) ?>" width="80">
+				</a>
+				<p class="p-name"><?php echo USER_NAME ?></p>
+				<p class="bio p-note"><?php echo USER_BIO ?></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="section container">
+		<div id="content" class="twelve columns offset-by-two">
+
+
 
 			<?php if ($alert = session_alert()) : ?>
 			<div class="infobox">
@@ -14,19 +98,20 @@
 			</div>
 			<?php endif; ?>
 
+
 			<?php if (isset($_SESSION['user'])) : ?>
-			<div class="post-form">
-				<div class="post-form-inner">
-				<form method="post" action="<?php echo SITE_BASE_URL ?>post">
-				<?php if (isset($individual_post)) : ?>
-					<textarea class="span9" rows="4" name="post[content]" placeholder="What's on your mind?"><?php if (isset($individual_post)) echo $posts[0]['raw'] ?></textarea>
-					<input type="hidden" name="post[id]" value="<?php echo $posts[0]['id'] ?>">
-					<div class="clearfix"><button type="submit" class="btn pull-right">Update</button></div>
-				<?php else: ?>
-					<textarea class="span9" rows="4" name="post[content]" placeholder="What's on your mind?"></textarea>
-					<div class="clearfix"><button type="submit" class="btn pull-right">Post</button></div>
-				<?php endif; ?>
-				</form>
+			<div>
+				<div class="post-box">
+					<form method="post" action="<?php echo SITE_BASE_URL ?>post">
+					<?php if (isset($individual_post)) : ?>
+						<textarea rows="4" name="post[content]" placeholder="What's on your mind?"><?php if (isset($individual_post)) echo $posts[0]['raw'] ?></textarea>
+						<input type="hidden" name="post[id]" value="<?php echo $posts[0]['id'] ?>">
+						<div><button type="submit" >Update</button></div>
+					<?php else: ?>
+						<textarea rows="4" name="post[content]" placeholder="What's on your mind?"></textarea>
+						<div><button type="submit" >Post</button></div>
+					<?php endif; ?>
+					</form>
 				</div>
 			</div>
 			<?php endif; ?>
@@ -43,76 +128,93 @@
 			<?php endif; ?>
 
 
-			<div class="posts">
-				<?php if ($posts) : ?>
-				<?php foreach ($posts as $post): ?>
-
-					<div class="media post h-entry">
-						<a class="pull-left p-author h-card" href="<?php echo SITE_BASE_URL ?>">
-						<img alt="Sandeep Shetty" src="<?php echo gravatar_url(USER_EMAIL, 420, 'mm', 'g', true) ?>" class="media-object img-polaroid" width="42" title="Sandeep Shetty" />
-						</a>
-						<div class="media-body">
-							<div class="post-content"><?php echo $post['content'] ?></div>
-							<div class="p-name p-summary e-content" style="display:none"><?php echo $post['plaintext'] ?></div>
-							<div class="post-more">
-								<a class="more-link u-url" title="Permalink" href="<?php echo SITE_BASE_URL.$post['id'] ?>"><time class="dt-published" datetime="<?php echo date(DATE_ATOM, strtotime($post['created_at'])); ?>"><?php echo date('j M Y', strtotime($post['created_at'])); ?></time></a>
-								<a class="more-link" title="Likes" href="<?php echo SITE_BASE_URL.$post['id'].'/likes' ?>"><i class="icon-thumbs-up"></i> <?php echo isset($mention_count[$post['id']]['like']) ? $mention_count[$post['id']]['like'] : 0 ?></a>
-								<a class="more-link" title="Reposts" href="<?php echo SITE_BASE_URL.$post['id'].'/reposts' ?>"><i class="icon-retweet"></i> <?php echo isset($mention_count[$post['id']]['repost']) ? $mention_count[$post['id']]['repost'] : 0 ?></a>
-								<a class="more-link" title="Mentions" href="<?php echo SITE_BASE_URL.$post['id'].'/mentions' ?>"><i class="icon-hand-right"></i> <?php echo isset($mention_count[$post['id']]['mention']) ? $mention_count[$post['id']]['mention'] : 0 ?></a>
-								<a class="more-link" title="Comments" href="<?php echo SITE_BASE_URL.$post['id'].'/comments' ?>"><i class="icon-comment"></i> <?php echo isset($mention_count[$post['id']]['in-reply-to']) ? $mention_count[$post['id']]['in-reply-to']: 0 ?></a>
-							</div>
-
-							<?php if (isset($_SESSION['user'])) : ?>
-							<div class="post-more">
-
-								<a href="https://twitter.com/share?url=<?php echo urlencode(SITE_BASE_URL.$post['id']) ?>&text=<?php echo urlencode($post['raw']) ?>" target="_blank">Share on Twitter</a>
-								&middot;
-								<a href="http://www.facebook.com/sharer.php?s=100&p[title]=<?php echo urlencode(ltrim($post['title'], '# ')) ?>&p[url]=<?php echo urlencode(SITE_BASE_URL.$post['id']) ?>&p[summary]=<?php echo urlencode($post['raw']) ?>" target="_blank">Share on Facebook</a>
-								&middot;
-								<a href="https://plus.google.com/share?url=<?php echo urlencode(SITE_BASE_URL.$post['id']) ?>" target="_blank">Share on Google+</a>
-								&middot;
-								<a href="<?php echo SITE_BASE_URL ?>send-webmention/<?php echo $post['id'] ?>" target="_blank">Send WebMentions</a>
-							</div>
-							<?php endif; ?>
-
-						</div>
-					</div>
-
-				<?php endforeach; ?>
-				<?php endif; ?>
-			</div>
 
 			<?php if ($posts) : ?>
-			<ul class="pager">
-				<?php if (!isset($individual_post) and isset($pager['after'])): ?>
-				<li class="previous"><a href="?after=<?php echo $pager['after'] ?>">&larr; Newer</a></li>
-				<?php elseif (isset($pager['after'])): ?>
-				<li class="previous"><a href="<?php echo SITE_BASE_URL.$pager['after'] ?>">&larr; Newer</a></li>
-				<?php endif; ?>
-				<?php if (!isset($individual_post) and isset($pager['before'])): ?>
-				<li class="next"><a href="?before=<?php echo $pager['before'] ?>">Older &rarr;</a></li>
-				<?php elseif (isset($pager['before'])): ?>
-				<li class="next"><a href="<?php echo SITE_BASE_URL.$pager['before'] ?>">Older &rarr;</a></li>
-				<?php endif; ?>
-			</ul>
+			<?php foreach ($posts as $post): ?>
+
+			<div class="h-entry">
+				<div class="post-box">
+					<div class="e-content">
+						<?php echo $post['content'] ?>
+					</div>
+					<div class="post-footer">
+						<a class="u-url" title="Permalink" href="<?php echo SITE_BASE_URL.$post['id'] ?>"><time class="dt-published" datetime="<?php echo date(DATE_ATOM, strtotime($post['created_at'])); ?>"><?php echo date('j M Y', strtotime($post['created_at'])); ?></time></a>
+					</div>
+				</div>
+			</div>
+
+			<?php endforeach; ?>
 			<?php endif; ?>
 
 		</div>
-
-		<div class="span3 sidebar">
-
-			<ul class="unstyled channels">
-				<li style="border-left: 10px solid #<?php echo dopplr_color("Home") ?>;"><a href="<?php echo SITE_BASE_URL ?>" class="channel <?php if (empty($channel_name) and !isset($individual_post)) echo 'active' ?>">Home</a></li>
-
-				<?php foreach ($channels as $channel): ?>
-					<?php if (!empty($channel_name) and ($channel_name == $channel['name'])): ?>
-					<li style="border-left: 10px solid #<?php echo dopplr_color($channel['name']) ?>;"><a class="channel active" href="<?php echo SITE_BASE_URL.$channel['name'].'/' ?>"><span class="deem">#</span><?php echo $channel['name'] ?></a></li>
-					<?php else: ?>
-					<li style="border-left: 10px solid #<?php echo dopplr_color($channel['name']) ?>;"><a class="channel" href="<?php echo SITE_BASE_URL.$channel['name'].'/' ?>"><!-- i class="icon-tag"></i --> <span class="deem">#</span><?php echo $channel['name'] ?></a></li>
-					<?php endif; ?>
-				<?php endforeach; ?>
-			</ul>
-
-		</div>
-
 	</div>
+
+	<?php if ($posts) : ?>
+	<div class="container pager">
+		<div class="twelve columns offset-by-two">
+			<?php if (!isset($individual_post) and isset($pager['after'])): ?>
+			<a class="newer" href="?after=<?php echo $pager['after'] ?>">&larr; Newer</a>
+			<?php elseif (isset($pager['after'])): ?>
+			<a class="newer" href="<?php echo SITE_BASE_URL.$pager['after'] ?>">&larr; Newer</a>
+			<?php endif; ?>
+			<?php if (!isset($individual_post) and isset($pager['before'])): ?>
+			<a class="older" href="?before=<?php echo $pager['before'] ?>">Older &rarr;</a>
+			<?php elseif (isset($pager['before'])): ?>
+			<a class="older" href="<?php echo SITE_BASE_URL.$pager['before'] ?>">Older &rarr;</a>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php endif; ?>
+
+
+<!-- End Document
+================================================== -->
+<script src="https://login.persona.org/include.js"></script>
+<script src="<?php echo SITE_BASE_URL ?>assets/js/jquery-1.8.2.min.js"></script>
+<script src="<?php echo SITE_BASE_URL ?>assets/js/jquery.fitvids.js"></script>
+
+<script>
+
+$(document).ready(function() {
+
+	$('#signin').click(function (e) {
+		e.preventDefault();
+		// TODO add returnTo: '/pathToReturnTo.html',
+		navigator.id.request({siteName: 'Converspace'});
+	});
+
+	$('#signout').click(function (e) {
+		e.preventDefault();
+		navigator.id.logout();
+	});
+
+	navigator.id.watch({
+		loggedInUser: $loggedInUser,
+		onlogin: function ($assertion) {
+			$.post(
+				'<?php echo SITE_BASE_URL ?>persona-verifier',
+				{assertion: $assertion},
+				function(data) {
+					window.location.reload();
+				}
+			);
+		},
+		onlogout: function () {
+			$.post(
+				'<?php echo SITE_BASE_URL ?>signout',
+				{},
+				function() {
+					window.location.reload();
+				}
+			);
+		}
+	});
+
+	$(".e-content").fitVids();
+
+});
+
+</script>
+
+</body>
+</html>
