2bb1240069edfbbb3f77e720a54ab7c26c5874d4
sandeepshetty <sandeep.shetty@gmail.com>
2013-06-20 09:34:50 +0530

    BOOM! Tying together blockquotes and mention posts into a common external content theme.

--- templates/post_preview.html.php
+++ templates/post_preview.html.php
@@ -1,6 +1,4 @@
 <?php use \Michelf\MarkdownExtra; ?>
-<div class="post-preview">
+<blockquote>
 	<?php echo MarkdownExtra::defaultTransform(@$machinetags[$mention['type']]['content']) ?>
-
-	&#8212; <a class="p-name" href="<?php echo @$machinetags[$mention['type']]['author_url'] ?>"><?php echo @$machinetags[$mention['type']]['author_name'] ?></a> on <a class="permalink <?php echo $mention['class'] ?>" href="<?php echo $machinetags[$mention['type']]['url'] ?>"><time class="" datetime=""><?php echo date('j M Y', strtotime(@$machinetags[$mention['type']]['published'])); ?></time></a>
-</div>
+</blockquote>
